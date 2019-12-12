9c40c482eb13cafdf9b4a2788687e2b67c255051
rjfm <javier.fuentesm@hotmail.com>
2019-12-12 00:36:04 -0600

    Se termina reto

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -4,4 +4,13 @@ describe("Calculos matematicos", () => {
   test("Prueba de sumas", () => {
     expect(sumar(1, 1)).toBe(2);
   });
+  test("Prueba de restas", () => {
+    expect(restar(1, 1)).toBe(0);
+  });
+  test("Prueba de multiplicar", () => {
+    expect(multiplicar(5, 2)).toBe(10);
+  });
+  test("Prueba de sumas", () => {
+    expect(dividir(10, 2)).toBe(5);
+  });
 });
