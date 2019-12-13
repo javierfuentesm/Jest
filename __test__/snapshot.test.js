a5d5bd77c05289cbd4c42da43046e95518258558
rjfm <javier.fuentesm@hotmail.com>
2019-12-13 13:47:27 -0600

    Test snapshots

---
+++ __test__/snapshot.test.js
@@ -0,0 +1,7 @@
+import {getCharacter} from '../snapshot'
+import rick from '../rick.json'
+describe('Es hora de las instantaneas', () => {
+    test('Snapshot', () => {
+        expect(getCharacter(rick)).toMatchSnapshot()
+    });
+})
