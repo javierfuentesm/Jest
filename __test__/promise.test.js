e00c1aae24c1c736f274b7469d3ae73ae54078a9
rjfm <javier.fuentesm@hotmail.com>
2019-12-13 13:04:52 -0600

    Test resolve y reject

---
+++ __test__/promise.test.js
@@ -0,0 +1,16 @@
+import { getDataFromApi } from "../promise";
+describe("Probando promesas", () => {
+  test("Peticion a una api", done => {
+    const api = "https://rickandmortyapi.com/api/character";
+    getDataFromApi(api).then(data => {
+      expect(data.results.length).toBeGreaterThan(0);
+      done();
+    });
+  });
+  test("Resuelve un Hola", () => {
+    return expect(Promise.resolve("Hola")).resolves.toBe("Hola");
+  });
+  test("Rechaza con un error", () => {
+    return expect(Promise.reject("Error")).rejects.toBe("Error");
+  });
+});
---
+++ promise.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const getDataFromApi = url => {
+  return axios.get(url).then(({ data }) => {
+    return data;
+  });
+};
