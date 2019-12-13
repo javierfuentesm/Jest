ba9145dabeb4ade919c21b58cc9d75894408f2f8
rjfm <javier.fuentesm@hotmail.com>
2019-12-12 20:41:35 -0600

    Se agregan pruabs de array

---
+++ __test__/array.test.js
@@ -0,0 +1,10 @@
+import { arrayFruits, arrayColors } from "../array";
+describe("Comprobaremos que existe un elemtno", () => {
+  test("¿Tiene una banana? ", () => {
+    expect(arrayFruits()).toContain("platano");
+  });
+
+  test("¿No contiene una banana? ", () => {
+    expect(arrayFruits()).not.toContain("banana");
+  });
+});
---
+++ array.js
@@ -0,0 +1,4 @@
+const fruits = ["platano", "melon", "sandia", "naranja", "limon", "pepino"];
+const colors = ["azul", "verde", "rojo", "rosa", "amarillo"];
+export const arrayFruits = () => fruits;
+export const arrayColors = () => colors;
