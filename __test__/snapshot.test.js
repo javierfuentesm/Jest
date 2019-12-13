d58ab30c361d3d96c845c2823cae940ef3a3316c
rjfm <javier.fuentesm@hotmail.com>
2019-12-13 14:01:48 -0600

    Excepciones de snapshots

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -4,4 +4,21 @@ describe('Es hora de las instantaneas', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot()
     });
+    test('Siempre fallara la isntantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random()*20)
+        }
+        expect(user).toMatchSnapshot()
+    });
+    test('Tenemos una excepcion dentro del snapshot', () => {
+        const user={
+            id: Math.floor(Math.random()*20),
+            name:"Javier Fuentes"
+
+        }
+        expect(user).toMatchSnapshot({
+            id:expect.any(Number)
+        })
+    });
 })
